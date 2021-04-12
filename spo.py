417f04885433ac5cfcb1ea8d316cbd2a81eb3be9
Penghui Huang <80626712+Penghuihuang2000@users.noreply.github.com>
2021-04-12 11:02:36 +0800

    Add files via upload

--- spo.py
+++ spo.py
@@ -4,9 +4,10 @@ class sporeads:
         import spotipy
         from spotipy.oauth2 import SpotifyClientCredentials
         self.artist = artist
+        # get the visiting credit
         sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="0822a31cd9d04068ba59343d6b3e1a80",
                                                                    client_secret="df0c7bbb087844179ff7264f1d715e5d"))
-        self.results = sp.search(q=artist, limit=5)
+        self.results = sp.search(q=artist, limit=5)  # attach the search result to sporeads
 
     def showreads(self):
         re = self.results
